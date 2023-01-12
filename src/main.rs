a09c0b0bc929feb816eaf6783b92b744b3cfcbb9
nasko.vasilev <nasko.vasilev@betvictor.com>
2023-01-12 23:07:46 +0100

    Add simple graphql client crate: gql_client and some minimalistic logic to test it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use gql_client::Client;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_with::serde_as;
 use std::collections::HashMap;
+use std::env;
 use time::format_description::well_known::Rfc3339;
 use time::OffsetDateTime;
 
@@ -13,10 +15,64 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     println!("{:#?}", resp);
 
-    let my_str = include_str!("gitlab_group_mrs_query.graphql");
-    print!("{my_str}");
+    // let qraphql_query = include_str!("gitlab_group_mrs_query.graphql");
+    let qraphql_query = include_str!("test.graphql");
+    println!("{qraphql_query}");
+
+    let endpoint = "https://gitlab.com/api/graphql";
+    let authorization_header = env::var("EM_TOKEN").unwrap_or("none".to_string());
+    println!("{authorization_header}");
+    let headers = HashMap::from([
+        ("Authorization", authorization_header.as_str()),
+        ("Content-Type", "application/json"),
+    ]);
+    let client = Client::new_with_headers(endpoint, headers);
+
+    let group_full_path = env::var("GROUP_FULL_PATH").unwrap_or("none".to_string());
+    println!("{group_full_path}");
+    let vars = GroupMergeRequestGraphQLVars {
+        group_full_path: group_full_path,
+    };
+    let data = client
+        .query_with_vars::<GroupData, GroupMergeRequestGraphQLVars>(qraphql_query, vars)
+        .await
+        .unwrap();
+
+    println!("Group Data: {:?}", data);
 
     Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    user: User,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct User {
+    id: String,
+    name: String,
+}
+
+#[derive(Serialize)]
+pub struct Vars {
+    id: u32,
+}
+
+#[derive(Serialize)]
+pub struct GroupMergeRequestGraphQLVars {
+    group_full_path: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GroupData {
+    group: Group,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Group {
+    id: String,
+    name: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
