02665fdaf8ac106cdd250f5000ba7b8b7c58e878
nasko.vasilev <nasko.vasilev@betvictor.com>
2023-01-22 21:12:15 +0100

    Add graphql_client crate and fetch group's merge request info

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,65 @@
-use gql_client::Client;
+use graphql_client::{reqwest::post_graphql, GraphQLQuery};
 use serde::Deserialize;
 use serde::Serialize;
 use serde_with::serde_as;
-use std::collections::HashMap;
 use std::env;
+use std::env::var;
 use time::format_description::well_known::Rfc3339;
 use time::OffsetDateTime;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
-
-    // let qraphql_query = include_str!("gitlab_group_mrs_query.graphql");
-    let qraphql_query = include_str!("test.graphql");
+    let qraphql_query = include_str!("gitlab_group_mrs_query.graphql");
     println!("{qraphql_query}");
 
     let endpoint = "https://gitlab.com/api/graphql";
-    let authorization_header = env::var("EM_TOKEN").unwrap_or("none".to_string());
+    let authorization_header = env::var("EM_TOKEN").unwrap().to_string();
     println!("{authorization_header}");
-    let headers = HashMap::from([
-        ("Authorization", authorization_header.as_str()),
-        ("Content-Type", "application/json"),
-    ]);
-    let client = Client::new_with_headers(endpoint, headers);
+    let updated_after = env::var("UPDATED_AFTER").unwrap().to_string();
+    let group_full_path = env::var("GROUP_FULL_PATH").unwrap().to_string();
 
-    let group_full_path = env::var("GROUP_FULL_PATH").unwrap_or("none".to_string());
-    println!("{group_full_path}");
-    let vars = GroupMergeRequestGraphQLVars {
-        group_full_path: group_full_path,
-    };
-    let data = client
-        .query_with_vars::<GroupData, GroupMergeRequestGraphQLVars>(qraphql_query, vars)
-        .await
-        .unwrap();
+    let mut has_more_merge_requests = true;
+    let mut after_pointer_token: core::option::Option<String> = None;
 
-    println!("Group Data: {:?}", data);
+    while has_more_merge_requests {
+        let variables = group_merge_reqs::Variables {
+            group_full_path: group_full_path.clone(),
+            updated_after: updated_after.clone(),
+            after: after_pointer_token.clone(),
+        };
+
+        let client = reqwest::Client::builder()
+            .user_agent("engineering-metrics-data-manager")
+            .default_headers(
+                std::iter::once((
+                    reqwest::header::AUTHORIZATION,
+                    reqwest::header::HeaderValue::from_str(&authorization_header).unwrap(),
+                ))
+                .collect(),
+            )
+            .build()?;
+        let response = post_graphql::<GroupMergeReqs, _>(&client, endpoint, variables).await?;
+
+        let response_data = response.data.expect("missing response data");
+        let group_data = response_data.group.unwrap();
+        println!("group_data: {:?}", &group_data);
+        after_pointer_token = group_data.merge_requests.page_info.end_cursor;
+        println!("after_pointer_token: {:?}", &after_pointer_token);
+        has_more_merge_requests = group_data.merge_requests.page_info.has_next_page;
+        println!("has_next_page: {:?}", &has_more_merge_requests);
+    }
 
     Ok(())
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Data {
-    user: User,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct User {
-    id: String,
-    name: String,
-}
-
-#[derive(Serialize)]
-pub struct Vars {
-    id: u32,
-}
-
-#[derive(Serialize)]
-pub struct GroupMergeRequestGraphQLVars {
-    group_full_path: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct GroupData {
-    group: Group,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Group {
-    id: String,
-    name: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct MergeRequest {}
+type Time = String;
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/gitlab_group_mrs_schema.graphql",
+    query_path = "src/gitlab_group_mrs_query.graphql",
+    response_derives = "Debug"
+)]
+struct GroupMergeReqs;
 
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug)]
