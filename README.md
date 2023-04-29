39caf8b60ba6bb2e5bf814ee7216dfcfd88a614b
nasko.vasilev <nasko.vasilev@betvictor.com>
2023-04-29 23:17:51 +0200

    add integration test to test the testcontainers for postgres

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # engineering-metrics-data-manager
+
+# testing
+
+`cargo test`
+
+## integration testing
+
+1. Start the local docker-engine, i.e. `colima start`
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,9 +1,10 @@
-use testcontainers::{clients};
+use testcontainers::clients;
+use sqlx::Row;
 
 mod postgres_container;
 
 #[tokio::test]
-async fn test_add() {
+async fn should_successfully_start_potgres_container() {
     let docker = clients::Cli::default();
     let image = postgres_container::Postgres::default();
     let node = docker.run(image);
@@ -12,3 +13,30 @@ async fn test_add() {
 
     assert_eq!(port > 1000, true);
 }
+
+#[tokio::test]
+async fn should_successfully_select_data_from_database() {
+    let docker = clients::Cli::default();
+    let image = postgres_container::Postgres::default();
+    let node = docker.run(image);
+    let port = node.get_host_port_ipv4(5432);
+    
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&format!("postgres://postgres:postgres@localhost:{}/postgres", port))
+        .await
+        .unwrap();
+
+    let mut conn = pool.acquire().await.unwrap();
+    let row = sqlx::query("SELECT 1 + 1 AS result")
+        .execute(&mut conn)
+        .await
+        .unwrap();
+    assert_eq!(row.rows_affected(), 1);
+
+    let row2 = sqlx::query("SELECT 1 + 1 AS result")
+        .fetch_one(&mut conn)
+        .await
+        .unwrap();
+    assert_eq!(row2.get::<i32, _>("result"), 2);
+}     
